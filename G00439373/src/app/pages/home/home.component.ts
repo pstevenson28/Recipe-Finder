bd7099fd2a3df68345dabfc2de2cc60955f198c5
Pearse Stevenson <pearsetstevenson@gmail.com>
2026-01-03 23:18:24 +0000

    Second commit of Recipe-Finder app

--- G00439373/src/app/pages/home/home.component.ts
+++ G00439373/src/app/pages/home/home.component.ts
@@ -1,35 +1,38 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';  // for ngModel binding
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RecipeService, RecipeSummary } from '../../services/recipe-service';  
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule],  // add FormsModule here
+  imports: [IonicModule, CommonModule, FormsModule, HttpClientModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-
   searchIngredients: string = '';
+  recipes: RecipeSummary[] = [];
 
-  // Fake recipes to display
-  recipes = [
-    {
-      id: 1,
-      title: 'Spaghetti Carbonara',
-      image: 'https://spoonacular.com/recipeImages/716429-312x231.jpg',
-    },
-    {
-      id: 2,
-      title: 'Chicken Alfredo',
-      image: 'https://spoonacular.com/recipeImages/715538-312x231.jpg',
-    },
-  ];
+  constructor(private recipeService: RecipeService) {}
 
   onSearch() {
-    
-    console.log('Searching for:', this.searchIngredients);
+    const trimmed = this.searchIngredients.trim();
+    if (!trimmed) {
+      this.recipes = [];
+      return;
+    }
+
+    this.recipeService.searchRecipes(trimmed).subscribe({
+      next: (response) => {
+        this.recipes = response.results;
+      },
+      error: (err) => {
+        console.error('API error', err);
+        this.recipes = [];
+      }
+    });
   }
 }
---
+++ G00439373/src/app/services/recipe-service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface RecipeSummary {
+  id: number;
+  title: string;
+  image: string;
+}
+
+@Injectable({
+  providedIn: 'root',  // Makes this service globally available
+})
+export class RecipeService {
+  private readonly apiKey = '70759a4f7911402abcc53d3c51d3b759';
+  private readonly apiUrl = 'https://api.spoonacular.com/recipes/complexSearch';
+
+  constructor(private http: HttpClient) {}
+
+  /**
+   * Search recipes by ingredients.
+   * @param ingredients A comma-separated string of ingredients.
+   * @returns An observable containing an object with a 'results' array of RecipeSummary.
+   */
+  searchRecipes(ingredients: string): Observable<{ results: RecipeSummary[] }> {
+    const params = new HttpParams()
+      .set('query', ingredients)
+      .set('apiKey', this.apiKey);
+
+    return this.http.get<{ results: RecipeSummary[] }>(this.apiUrl, { params });
+  }
+}
